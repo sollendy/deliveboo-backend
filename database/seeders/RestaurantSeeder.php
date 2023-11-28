4078a1b3d1e104519a119a7a6217d46f3fbd7b0f
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-11-28 16:45:36 +0100

    added restaurant seeder data

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
 
 class RestaurantSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class RestaurantSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $restaurants = config("restaurantsDB");
+
+        foreach ($restaurants as $index => $restaurant) {
+            $newRestaurant = new Restaurant();
+            $newRestaurant->user_id = $index + 1;
+            $newRestaurant->name = $restaurant['name'];
+            $newRestaurant->address = $restaurant['address'];
+            $newRestaurant->photo = $restaurant['photo'];
+            $newRestaurant->save();
+        }
     }
 }

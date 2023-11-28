a5144e8beb6257d3d9d7c55b44d36b3e9848eefa
raffaelecapaldo <raffaelecpl@gmail.com>
2023-11-28 18:07:36 +0100

    added Order Seeder and Data

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $orders = config("ordersData");
+
+        foreach ($orders as $order) {
+            $newOrder = new Order();
+            $newOrder->name = $order['name'];
+            $newOrder->last_name = $order['last_name'];
+            $newOrder->address = $order['address'];
+            $newOrder->phone = $order['phone'];
+            $newOrder->status = rand(0,1);
+            $newOrder->total_price = 1;
+            $newOrder->save();
+
+
+
+
+        }
     }
 }

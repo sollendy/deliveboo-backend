23699b9c5f840f9aad5963a9b358b5869d4569f7
raffaelecapaldo <raffaelecpl@gmail.com>
2023-11-28 16:17:48 +0100

    added data of types

---
+++ config/dataTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    ["name" => "Cucina italiana"],
+    ["name" => "Messicano"],
+    ["name" => "Cucina americana"],
+    ["name" => "Giapponese"],
+    ["name" => "Cinese"],
+    ["name" => "Fast food"],
+    ["name" => "Vegetariano"],
+    ["name" => "Vegano"],
+    ["name" => "Ristorante di pesce"],
+    ["name" => "Grill"],
+    ["name" => "Pizzeria"],
+    ["name" => "Tapas"],
+    ["name" => "Caffetteria"],
+    ["name" => "Gelateria"]
+
+];

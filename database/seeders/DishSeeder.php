6ebf2e5b3c36094121e627a28d8054cec3baf065
sollendy <edlannino@tutanota.com>
2023-11-28 16:51:58 +0100

    aggiunto seeder piatti

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dish;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class DishSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $dishes = config("dataDishes");
+
+        foreach($dishes as $dish) {
+            $newDish = new Dish();
+            
+            $newDish->name = $dish["name"];
+            $newDish->description = $dish["description"];
+            $newDish->ingredients = $dish["ingredients"];
+            $newDish->visible = rand(0, 1);
+            $newDish->price = $dish["price"];
+        }
     }
 }

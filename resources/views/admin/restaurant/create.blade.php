ffa31feea0484f8fe42001537878e473e0e384af
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-12-04 17:25:45 +0100

    test

--- resources/views/admin/restaurant/create.blade.php
+++ resources/views/admin/restaurant/create.blade.php
@@ -4,63 +4,62 @@
 
 @section('content')
 
-@if ($errors->any())
+    @if ($errors->any())
+        <div class="alert alert-danger">
 
-    <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
 
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
 
-        <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
 
-            @foreach ($errors->all() as $error)
+            </ul>
 
-                <li>{{ $error }}</li>
-
-            @endforeach
-
-        </ul>
-
-    </div>
-
-@endif
+        </div>
+    @endif
 
 
 
-<form action="{{ route('admin.restaurant.store') }}" method="POST">
+    <form action="{{ route('admin.restaurant.store') }}" method="POST">
 
-    @csrf
+        @csrf
 
 
-     <div class="row">
+        <div class="row">
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-12">
 
-            <div class="form-group">
+                <div class="form-group">
 
-                <input type="text" name="name" class="form-control" placeholder="Name" required minlength="5" maxlength="100">
+                    <input type="text" name="name" class="form-control" placeholder="Name" required minlength="5"
+                        maxlength="100">
 
-                <textarea name="description" id="description" cols="30" rows="10" minlength="50" placeholder="description"></textarea>
+                    <textarea name="description" id="description" cols="30" rows="10" minlength="50" placeholder="description"></textarea>
 
-                <input type="text" name="ingredients" class="form-control" placeholder="ingredients" required minlength="25">
-                <label for="visible" class="ms-1">Visibile</label>
-                <input type="checkbox" id="visible" name="visible"  placeholder="visible">
+                    <input type="text" name="ingredients" class="form-control" placeholder="ingredients" required
+                        minlength="25">
+                    <label for="visible" class="ms-1">Visibile</label>
+                    <input type="checkbox" id="visible" name="visible" placeholder="visible">
 
-                <input type="number" name="price" class="form-control" min="0" value="0.00" step="0.01" placeholder="price" required>
+                    <input type="number" name="price" class="form-control" min="0" value="0.00" step="0.01"
+                        placeholder="price" required>
+
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+                <button type="submit" class="btn btn-primary mt-5">Submit</button>
 
             </div>
 
         </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            {{-- TEST PUSH --}}
 
-                <button type="submit" class="btn btn-primary mt-5">Submit</button>
-
-        </div>
-
-    </div>
-
-
-
-</form>
+    </form>
 
 @endsection

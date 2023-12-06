b148698e819a4aca21c1848d2c1268669310c3c5
raffaelecapaldo <raffaelecpl@gmail.com>
2023-12-06 16:46:43 +0100

    new names for the categoris of "cucina"

--- config/dataTypes.php
+++ config/dataTypes.php
@@ -2,15 +2,15 @@
 
 return [
 
-    ["name" => "Cucina italiana"],
-    ["name" => "Messicano"],
-    ["name" => "Cucina americana"],
+    ["name" => "Italiana"],
+    ["name" => "Messicana"],
+    ["name" => "Americana"],
     ["name" => "Giapponese"],
     ["name" => "Cinese"],
-    ["name" => "Fast food"],
-    ["name" => "Vegetariano"],
-    ["name" => "Vegano"],
-    ["name" => "Ristorante di pesce"],
+    ["name" => "Fastfood"],
+    ["name" => "Vegetariana"],
+    ["name" => "Vegana"],
+    ["name" => "Pesce"],
     ["name" => "Grill"],
     ["name" => "Pizzeria"],
     ["name" => "Tapas"],
